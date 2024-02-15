a67c065645f8cb97cc6c2b4f641969f7ff33ba43
Changhui Xu <changhuixu@hotmail.com>
2024-02-15 13:36:06 -0600

    todo app: add validation and exceptions

--- Module05/todo_app/todo.py
+++ Module05/todo_app/todo.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Path, status
+from fastapi.responses import JSONResponse
 
 from model import Todo
 
@@ -7,12 +9,30 @@ todo_router = APIRouter()
 todo_list = []
 
 
-@todo_router.post("/todos")
-async def add_todo(todo: Todo) -> dict:
+@todo_router.post(
+    "/todos",
+    status_code=status.HTTP_201_CREATED,
+)
+async def add_todo(todo: Todo) -> Todo:
     todo_list.append(todo)
-    return {"msg": "new todo added"}
+    json_obj = todo.model_dump()
+    return JSONResponse(json_obj, status_code=status.HTTP_201_CREATED)
 
 
 @todo_router.get("/todos")
 async def get_todos() -> dict:
     return {"todos": todo_list}
+
+
+@todo_router.get("/todos/{id}")
+async def get_todo_by_id(
+    id: Annotated[int, Path(title="The ID  to get", ge=0, le=1000)]
+) -> dict:
+    for todo in todo_list:
+        if todo.id == id:
+            return {"todo": todo}
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"The todo with ID={id} is not found",
+    )
